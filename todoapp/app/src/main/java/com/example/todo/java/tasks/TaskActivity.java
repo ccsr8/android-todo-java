09a41d1e42ad129790f12ce9bbc10745e855416e
Chatchai <chatchai4566@gmail.com>
2019-06-09 21:38:22 +0700

    Edited -  add member

--- todoapp/app/src/main/java/com/example/todo/java/tasks/TaskActivity.java
+++ todoapp/app/src/main/java/com/example/todo/java/tasks/TaskActivity.java
@@ -3,14 +3,32 @@ package com.example.todo.java.tasks;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.todo.java.R;
 
 public class TaskActivity extends AppCompatActivity {
+
+    //region [public members]
+
+    public static final String TASKS_VIEWMODEL_TAG = "TASKS_VIEWMODEL_TAG";
+    private DrawerLayout mDrawerLayout;
+    private TaskViewModel mViewModel;
+
+
+    //endregion
+
+    //region [private members]
+
+    //endregion
+
+    //region [event handler]
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
     }
+
+    //endregion
 }
