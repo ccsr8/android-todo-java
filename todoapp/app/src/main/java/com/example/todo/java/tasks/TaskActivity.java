4e753e499d17d7257274437cfa8e3ab19bf307dd
Chatchai <chatchai4566@gmail.com>
2019-06-09 15:19:32 +0700

    Created - Initial file

---
+++ todoapp/app/src/main/java/com/example/todo/java/tasks/TaskActivity.java
@@ -0,0 +1,16 @@
+package com.example.todo.java.tasks;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.todo.java.R;
+
+public class TaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+    }
+}
